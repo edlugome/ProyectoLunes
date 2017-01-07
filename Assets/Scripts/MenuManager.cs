be080c9b525f86bb1df3c06fe50e9a0b8c5dea51
TAKE\Take <takeukoo@gmail.com>
2017-01-06 21:27:17 -0400

    Main Screen

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class MenuManager : MonoBehaviour 
+{
+	public Menu CurrentMenu;
+	public void Start()
+	{
+		ShowMenu (CurrentMenu);
+	}
+
+
+	public void ShowMenu(Menu menu)
+	{
+		if (CurrentMenu != null) 
+		{
+			CurrentMenu.IsOpen = false;
+
+			CurrentMenu = menu;
+			CurrentMenu.IsOpen = true;
+		}
+
+	}
+
+
+
+}
+
